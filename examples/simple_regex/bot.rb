3cfd0bec93ade0001220fceb93112bac17a3a5e0
Aboobacker MK <aboobackervyd@gmail.com>
2017-04-19 07:49:41 +0530

    example using regex recognizer

---
+++ examples/simple_regex/bot.rb
@@ -0,0 +1,30 @@
+BotFramework.configure do |connector|
+  connector.app_id = ENV['RUBY_CONF_MICROSOFT_APP_ID']
+  connector.app_secret = ENV['RUBY_CONF_MICROSOFT_APP_PASSWORD']
+end
+
+BotFramework::Bot.recognizer = BotFramework::Dialogs::RegExpRecognizer.new('HelpIntent', /^(help|options)/i)
+
+BotFramework::Bot.on :activity do |activity|
+  # All activities
+end
+
+BotFramework::Bot.on :message do |activity|
+  # Call back for messages
+end
+
+BotFramework::Bot.on :ping do |activity|
+  # Callback for pings
+end
+
+BotFramework::Bot.on :typing do |activity|
+  # Callback for typing
+end
+
+BotFramework::Bot.on_intent 'HelpIntent' do |activity, _intents|
+  reply(activity, "<HELP TEXT HERE>")
+end
+
+BotFramework::Bot.on_intent :default do |activity, _intents|
+  reply(activity, 'Unable to recognize')
+end
---
+++ spec/bot_framework/dialogs/reg_exp_recognizer_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+module BotFramework
+  module Dialogs
+    describe RegExpRecognizer do
+      it 'set attribute accessors' do
+        recognizer = RegExpRecognizer.new('test',/test/)
+        expect(recognizer.expressions).to eq({'*': /test/})
+        expect(recognizer.intent).to eq('test')
+      end
+
+      it '#recognize' do
+        recognizer = RegExpRecognizer.new('test',/test/)
+        expect {recognizer.recognize(message: {text: 'test'}) }.to raise_error(LocalJumpError)
+      end
+    end
+  end
+end
